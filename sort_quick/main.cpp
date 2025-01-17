687be049b95589467ef5afc08d0ef1cc44fc2e55
Max <Max03032015Vlad@gmail.com>
2025-01-17 22:47:41 +0200

    changed quick sort

--- sort_quick/main.cpp
+++ sort_quick/main.cpp
@@ -9,17 +9,18 @@ using namespace std;
 void sort_quick(int* ar, int L, int R) {
     if (L >=R)
         return;
-
     int m = ar[(L + R) / 2];
     int i = L;
     int j = R;
 
-    while (i <= j) {
+    while (i <= j)
+    {
         while (ar[i] < m)
             i++;
         while (ar[j] > m)
             j--;
-        if (i <= j) {
+        if (i <= j)
+        {
             swap(ar[i], ar[j]);
             i++; 
             j--; 
@@ -31,20 +32,22 @@ void sort_quick(int* ar, int L, int R) {
 }
 
 int main() {
-    std::ifstream in("03.in");
-    std::ofstream out("03.out");
+    std::ifstream in("input.txt");
+    std::ofstream out("output.txt");
     int n;
     in >> n;
 
     int mas[3'000'000];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         in >> mas[i];
     }
 
     sort_quick(mas, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         out << mas[i] << " ";
     }
 
@@ -52,3 +55,14 @@ int main() {
     out.close();
     return 0;
 }
+/*
+quick sort:
+    в среднем случае O( nlog(n) )
+    в худшем случае O(n^2)
+
+swap sort:
+    всегда O(n^2)
+*/
+
+
+
